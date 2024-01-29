71e0ad8d949d746aa399af7ecac24e7d64961e59
IgorVVieira <igor.gutoo63@gmail.com>
2024-01-29 10:49:30 -0300

    feat: add ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/sign-up.ts
@@ -0,0 +1,12 @@
+export class SignUpController {
+  handle (httpRequest: any): any {
+    // if (!httpRequest.body.name) {
+    //   return {
+    //     statusCode: 400,
+    //   }
+    // }
+    return {
+      statusCode: 400
+    }
+  }
+}
