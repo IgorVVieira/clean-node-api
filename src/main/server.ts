86029b54f35f31affb58802d1a71e087c65f5406
IgorVVieira <igor.gutoo63@gmail.com>
2024-02-15 16:13:24 -0300

    feat: add body parser middleware

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,7 @@
 // Camada onde se cria todas instancias de objetos e configurações do servidor
 // Onde sera definido os factories e middlewares
 
-import express from 'express'
-
-const app = express()
+import app from './config/app'
 
 const port = process.env.PORT ?? 3333
 app.listen(port, () => {
