3a65164a06555ccd8e8e605fabe32614bf8d1303
IgorVVieira <igor.gutoo63@gmail.com>
2024-02-15 13:56:18 -0300

    test: ensure AccountMongoRepository returns an account on sucess

---
+++ src/infra/db/mongodb/account-respository/account.spec.ts
@@ -0,0 +1,31 @@
+import { MongoHelper } from '../helper/mongo'
+import { AccountMongoRepository } from './account'
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(String(process.env.MONGO_URL))
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const mockAccount = {
+      name: 'any_name',
+      email: 'any_mail@mail.com',
+      password: 'any_password'
+    }
+    const account = await sut.add(mockAccount)
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(mockAccount.name)
+    expect(account.email).toBe(mockAccount.email)
+  })
+})
