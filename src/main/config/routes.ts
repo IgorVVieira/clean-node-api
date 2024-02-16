6a6cc1783e5ee90e09e7b489afb976b23181e36f
IgorVVieira <igor.gutoo63@gmail.com>
2024-02-16 11:46:46 -0300

    feat: ensure SignUp route ruturns  an account on sucess

---
+++ src/main/config/routes.ts
@@ -0,0 +1,13 @@
+import { type Express, Router } from 'express'
+import fastGlob from 'fast-glob'
+
+export default async (app: Express): Promise<void> => {
+  const router = Router()
+  app.use('/api', router)
+
+  const routeFiles = fastGlob.sync('**/src/main/routes/**routes.ts')
+
+  await Promise.all(
+    routeFiles.map(async (file) => (await import(`../../../${file}`)).default(router))
+  )
+}
