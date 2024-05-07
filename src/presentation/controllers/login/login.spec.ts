972bef7fe60f012aaa1e235f26707e19672d0ff4
IgorVVieira <igor.gutoo63@gmail.com>
2024-05-07 18:42:24 -0300

    test: ensure LoginController call emailValidator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,11 +2,19 @@ import { badRequest } from '@presentation/helpers/http-helper'
 import { LoginController } from './login'
 import { MissinParamError } from '@presentation/errors'
 
+const emailValidator = {
+  isValid: jest.fn()
+}
+
 const makeSut = (): LoginController => {
-  return new LoginController()
+  return new LoginController(emailValidator)
 }
 
 describe('Login Controller', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    emailValidator.isValid.mockReturnValue(true)
+  })
   test('Should return 400 if no email is provided', async () => {
     const sut = makeSut()
 
@@ -18,6 +26,34 @@ describe('Login Controller', () => {
 
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissinParamError('email')))
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const sut = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissinParamError('password')))
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    const sut = makeSut()
+    const isValidSpy = jest.spyOn(emailValidator, 'isValid')
+
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email')
   })
 
   test('Should return 400 if no password is provided', async () => {
