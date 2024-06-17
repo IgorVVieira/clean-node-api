594a2961f21edb6eed696d223051df83ae2e40ec
IgorVVieira <igor.gutoo63@gmail.com>
2024-06-17 19:55:02 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,4 +1,4 @@
-import { badRequest, sucesss } from '@presentation/helpers/http-helper'
+import { badRequest, serverError, sucesss } from '@presentation/helpers/http-helper'
 import { HttpRequest, HttpResponse, IController, IEmailValidator } from '../signup/sign-up-protocols'
 import { InvalidParamError, MissingParamError } from '@presentation/errors'
 
@@ -6,23 +6,27 @@ export class LoginController implements IController {
   public constructor(private readonly emailValidator: IEmailValidator) { }
 
   public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
-    }
-    if (!password) {
-      return await new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))) })
-    }
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
+      }
+      if (!password) {
+        return await new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))) })
+      }
 
-    const isValidEmail = this.emailValidator.isValid(email as string)
-    if (!isValidEmail) {
-      return await new Promise(resolve => { resolve(badRequest(new InvalidParamError('email'))) })
-    }
+      const isValidEmail = this.emailValidator.isValid(email as string)
+      if (!isValidEmail) {
+        return await new Promise(resolve => { resolve(badRequest(new InvalidParamError('email'))) })
+      }
 
-    return await new Promise(resolve => {
-      resolve(sucesss({
-        email, password
-      }))
-    })
+      return await new Promise(resolve => {
+        resolve(sucesss({
+          email, password
+        }))
+      })
+    } catch (error) {
+      return serverError(error as Error)
+    }
   }
 }
