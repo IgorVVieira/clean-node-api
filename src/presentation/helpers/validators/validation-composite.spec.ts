96c0ba63108c183790b4d5b60978d9d72b170b62
IgorVVieira <igor.gutoo63@gmail.com>
2024-07-02 19:27:27 -0300

    test: ensure tests for validationComposite

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,32 @@
+import { MissingParamError } from '@presentation/errors'
+import { ValidationComposite } from './validation-composite'
+
+const validationMock1 = {
+  validate: jest.fn()
+}
+
+const validationMock2 = {
+  validate: jest.fn()
+}
+
+describe('ValidationComposite', () => {
+  it('should return an error if one of the validations fails', () => {
+    validationMock1.validate.mockReturnValueOnce(new MissingParamError('field'))
+    validationMock2.validate.mockReturnValueOnce(undefined)
+
+    const sut = new ValidationComposite([validationMock1, validationMock2])
+
+    const input = {}
+    expect(sut.validate(input)).toEqual(new MissingParamError('field'))
+  })
+
+  it('should return undefined if all validations pass', () => {
+    validationMock1.validate.mockReturnValueOnce(undefined)
+    validationMock2.validate.mockReturnValueOnce(undefined)
+
+    const sut = new ValidationComposite([validationMock1, validationMock2])
+
+    const input = {}
+    expect(sut.validate(input)).toBeUndefined()
+  })
+})
