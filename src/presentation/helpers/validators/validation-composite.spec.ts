87bfecba5f3e3fdcf01649c657b1a9cc060b7a44
IgorVVieira <igor.gutoo63@gmail.com>
2024-07-02 19:32:36 -0300

    test: esnure ValidationComposite returns the first error if multiple validations fail

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '@presentation/errors'
+import { InvalidParamError, MissingParamError } from '@presentation/errors'
 import { ValidationComposite } from './validation-composite'
 
 const validationMock1 = {
@@ -18,6 +18,16 @@ describe('ValidationComposite', () => {
 
     const input = {}
     expect(sut.validate(input)).toEqual(new MissingParamError('field'))
+  })
+
+  it('should return the first error if multiple validations fail', () => {
+    validationMock1.validate.mockReturnValueOnce(new MissingParamError('field1'))
+    validationMock2.validate.mockReturnValueOnce(new InvalidParamError('field2'))
+
+    const sut = new ValidationComposite([validationMock1, validationMock2])
+
+    const input = {}
+    expect(sut.validate(input)).toEqual(new MissingParamError('field1'))
   })
 
   it('should return undefined if all validations pass', () => {
